51c2ae58acf1dd896521000f134827b3488cf256
sameenshafeeq <sameenshafeeq123@gmail.com>
2022-07-14 22:28:32 -0400

    Add files via upload

---
+++ Javascript_related_work/script.js
@@ -0,0 +1,9 @@
+function getValues()
+{
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let about=document.getElementById("about").value;
+    console.log("name:"+ name);
+    console.log("email:"+ email);
+    console.log("about:"+ about);
+}
